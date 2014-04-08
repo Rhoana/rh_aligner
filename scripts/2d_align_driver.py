c60fde73aff9dac6305188a69fadfbe076444b29
Adi <adi.suissa@gmail.com>
2014-04-08 15:49:16 -0400

    Adding a 2d_align_driver that will run the entire alignment process for each 2D layer, and updating the filter_tiles and create_sift_features scripts so they can be executed by it.

---
+++ scripts/2d_align_driver.py
@@ -0,0 +1,59 @@
+# A driver for running 2D alignment using the FijiBento alignment project
+# The input is a directory that contains image files (tiles), and the output is a 2D montage of these files
+# Activates ComputeSIFTFeaturs -> MatchSIFTFeatures -> OptimizeMontageTransfrom
+# and the result can then be rendered if needed
+#
+# requires:
+# - java (executed from the command line)
+# - 
+
+import sys
+import os
+import argparse
+from subprocess import call
+import urllib
+import urlparse
+import json
+
+from filter_tiles import filter_tiles
+from create_sift_features import create_sift_features
+
+# Command line parser
+parser = argparse.ArgumentParser(description='A driver that does a 2D alignment of images.')
+parser.add_argument('tiles_fname', metavar='tiles_json', type=str, 
+                	help='a tile_spec file that contains all the images to be aligned in json format')
+parser.add_argument('-w', '--workspace_dir', type=str, 
+                	help='a directory where the output files of the different stages will be kept (default: current directory)',
+                	default='.')
+parser.add_argument('-r', '--render', action='store_true',
+					help='render final result')
+parser.add_argument('-o', '--output_file_name', type=str, 
+                	help='the file that includes the output to be rendered in json format (default: output.json)',
+                	default='output.json')
+parser.add_argument('-j', '--jar_file', type=str, 
+                	help='the jar file that includes the render (default: ../target/render-0.0.1-SNAPSHOT.jar)',
+                	default='../target/render-0.0.1-SNAPSHOT.jar')
+# the default bounding box is as big as the image can be
+parser.add_argument('-b', '--bounding_box', type=str, 
+                	help='the bounding box of the part of image that needs to be aligned format: "from_x to_x from_y to_y" (default: all tiles)',
+                	default='{0} {1} {2} {3}'.format((-sys.maxint - 1), sys.maxint, (-sys.maxint - 1), sys.maxint))
+
+args = parser.parse_args()
+
+print args
+
+# create a workspace directory if not found
+if not os.path.exists(args.workspace_dir):
+	os.makedirs(args.workspace_dir)
+
+# filter the tiles to the requested bounding box
+filter_dir = os.path.join(args.workspace_dir, "filterd")
+filter_tiles(args.tiles_fname, filter_dir, args.bounding_box)
+
+
+# create the sift features of these tiles
+sift_dir = os.path.join(args.workspace_dir, "sifts")
+create_sift_features(filter_dir, sift_dir, args.jar_file)
+
+# match the features of overlapping tiles
+
