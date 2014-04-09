07ddfb0864e1eb76d03ea48966207e8bae06a5ee
Seymour Knowles-Barley <seymourkb@seas.harvard.edu>
2014-04-08 20:38:52 -0400

    bugfix

--- src/main/java/org/janelia/alignment/TileSpec.java
+++ src/main/java/org/janelia/alignment/TileSpec.java
@@ -22,7 +22,7 @@ import mpicbg.models.CoordinateTransformList;
 /**
  * 
  *
- * @author Stephan Saalfeld <saalfelds@janelia.hhmi.org>
+ * @author Stephan Saalfeld <saalfelds@janelia.hhmi.org>, Seymour Knowles-Barley
  */
 public class TileSpec
 {
@@ -31,10 +31,10 @@ public class TileSpec
 	public double minIntensity = 0;
 	public double maxIntensity = 255;
 	// width and height of base tile image
-	public double width = 0;
-	public double height = 0;
+	public int width = 0;
+	public int height = 0;
 	// bounding box after transformations are applied [left, right, top, bottom] possibly with extra entries for [front, back, etc.]
-	public double[] bbox = null;
+	public int[] bbox = null;
 	public Transform[] transforms = null;
 	
 	final public CoordinateTransformList< CoordinateTransform > createTransformList()
