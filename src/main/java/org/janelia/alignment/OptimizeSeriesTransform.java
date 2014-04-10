8d8dea765a6adfe948106c802db78df80fbd0c54
Seymour Knowles-Barley <seymourkb@seas.harvard.edu>
2014-04-09 20:40:05 -0400

    bug fix and more output messages

--- src/main/java/org/janelia/alignment/OptimizeSeriesTransform.java
+++ src/main/java/org/janelia/alignment/OptimizeSeriesTransform.java
@@ -426,6 +426,8 @@ J:		for ( int i = 0; i < corr_data.length; )
 //			}
 //		}
 			
+		System.out.println( "Optimization complete. Generating tile transforms.");
+		
 		ArrayList< TileSpec > out_tiles = new ArrayList< TileSpec >();
 		
 		// Export new transforms, TODO: append to existing tilespec files
@@ -446,14 +448,19 @@ J:		for ( int i = 0; i < corr_data.length; )
 			{
 			case 0:
 				addedTransform.dataString = ((TranslationModel2D) genericModel).toDataString();
+				break;
 			case 1:
 				addedTransform.dataString = ((RigidModel2D) genericModel).toDataString();
+				break;
 			case 2:
 				addedTransform.dataString = ((SimilarityModel2D) genericModel).toDataString();
+				break;
 			case 3:
 				addedTransform.dataString = ((AffineModel2D) genericModel).toDataString();
+				break;
 			case 4:
 				addedTransform.dataString = ((HomographyModel2D) genericModel).toDataString();
+				break;
 			default:
 				addedTransform.dataString = genericModel.toString();
 			}		    
@@ -462,6 +469,8 @@ J:		for ( int i = 0; i < corr_data.length; )
 		    
 		    out_tiles.add(ts);
 		}
+		
+		System.out.println( "Exporting tiles.");
 		
 		try {
 			Writer writer = new FileWriter(params.targetPath);
