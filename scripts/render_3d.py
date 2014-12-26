08069ebc34f7ef15c4142948bfd600f304b640c7
Adi <adi.suissa@gmail.com>
2014-12-26 10:34:49 -0500

    Refixing rendering scripts

--- scripts/render_3d.py
+++ scripts/render_3d.py
@@ -20,7 +20,7 @@ def render_3d(tile_fnames_or_dir_fname, output_dir, from_layer, to_layer, width,
 def main():
     # Command line parser
     parser = argparse.ArgumentParser(description='Given a list of tilespec file names, normalizes all the tilepecs to a single coordinate system starting from (0,0).')
-    parser.add_argument('tile_files_or_dirs_fname', metavar='tile_files_or_dirs_fname', type=str, nargs='+',
+    parser.add_argument('tile_files_or_dirs_fname', metavar='tile_files_or_dirs_fname', type=str, 
                         help='a list of json files that need to be normalized or a file that contains the list of json files')
     parser.add_argument('-o', '--output_dir', type=str, 
                         help='an output directory (default: ./after_render)',
