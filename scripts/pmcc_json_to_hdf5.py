aadbed06847aad39b4d4819a0f954435f59575de
Adi Suissa-Peleg <adisuis@seas.harvard.edu>
2015-04-27 12:07:01 -0400

    Conversion to hdf5 using multiple processes

--- scripts/pmcc_json_to_hdf5.py
+++ scripts/pmcc_json_to_hdf5.py
@@ -2,7 +2,10 @@ import h5py
 import ujson
 import numpy as np
 import sys
-import os.path
+import os
+import multiprocessing as mp
+import glob
+import utils
 
 def convert(file_in, file_out):
     data = ujson.load(open(file_in, "r"))
@@ -23,5 +26,52 @@ def convert(file_in, file_out):
             hf.create_dataset("matches_{}_p1".format(idx), data=p1s)
             hf.create_dataset("matches_{}_p2".format(idx), data=p2s)
 
+def create_chunks(l, n):
+    sub_size = max(1, (len(l) - 1)/n + 1)
+    return [l[i:i + sub_size] for i in range(0, len(l), sub_size)]
+
+def convert_files(files_list, json_dir, hdf5_dir):
+    for f in files_list:
+        in_file = os.path.join(json_dir, f)
+        print "parsing", in_file
+        out_file = os.path.join(hdf5_dir, "{}.hdf5".format(os.path.splitext(os.path.basename(f))[0]))
+        if not os.path.exists(out_file):
+            convert(in_file, out_file)
+
+def parallel_convert(processes_num, json_dir, hdf5_dir):
+    utils.create_dir(hdf5_dir)
+
+    # Get the json files list
+    json_files = glob.glob(os.path.join(json_dir, '*pmcc.json'))
+
+    # create N-1 worker processes
+    if processes_num > 1:
+        pool = mp.Pool(processes=processes_num - 1)
+    print "Creating {} other processes, and parsing {} json files".format(processes_num - 1, len(json_files))
+
+    # Divide the list into processes_num chunks
+    chunks = create_chunks(json_files, processes_num)
+
+    async_res = None
+
+    # run all jobs but one by other processes
+    for sub_list in chunks[:-1]:
+        async_res = pool.apply_async(convert_files, (sub_list, json_dir, hdf5_dir))
+
+    # run the last job by the current process
+    print "running last list with {} files".format(len(chunks[-1]))
+    convert_files(chunks[-1], json_dir, hdf5_dir)
+
+    # wait for all other processes to finish their job
+    if processes_num > 1:
+        if pool is not None:
+            pool.close()
+            pool.join()
+
+
 if __name__ == '__main__':
-    convert(sys.argv[1], sys.argv[2])
+    processes_num = int(sys.argv[1])
+    json_dir = sys.argv[2]
+    hdf5_dir = sys.argv[3]
+    parallel_convert(processes_num, json_dir, hdf5_dir)
+
