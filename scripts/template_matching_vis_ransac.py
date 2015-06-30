25bea975eedb979543f6d2ec83a387887dd4a851
Thouis (Ray) Jones <thouis@gmail.com>
2015-06-29 21:17:26 -0400

    simplify code, speed it up

--- scripts/template_matching_vis_ransac.py
+++ scripts/template_matching_vis_ransac.py
@@ -8,38 +8,32 @@ import json
 import math
 import sys
 from scipy.spatial import distance
-import cv2
 import time
 import glob
 import matplotlib.pyplot as plt
-from pylab import axis
+from matplotlib import collections  as mc
 
 def main():
     script, jsonfile = sys.argv
     with open(jsonfile) as data_file1:
             data1 = json.load(data_file1)
-    
+
     pms = data1['pointmatches']
-    pointmatches = []
-    for i in range(0, len(pms)):
-        pointmatches.append((np.array(pms[i]['point1']), np.array(pms[i]['point2'])))
+    points1 = np.array([p['point1'] for p in pms])
+    points2 = np.array([p['point2'] for p in pms])
+
+    assert points1.shape[1] == 2
     
-    point1s = map(list, zip(*pointmatches))[0]
-    point1r = map(list, zip(*pointmatches))[0]
-    point1s = map(lambda x: np.matrix(x).T, point1s)
-    point2s = map(list, zip(*pointmatches))[1]
-    point2r = map(list, zip(*pointmatches))[1]
-    point2s = map(lambda x: np.matrix(x).T, point2s)
-    centroid1 = [np.array(point1s)[:,0].mean(), np.array(point1s)[:,1].mean()]
-    centroid2 = [np.array(point2s)[:,0].mean(), np.array(point2s)[:,1].mean()]
+    centroid1 = points1.mean(axis=0, keepdims=True)
+    centroid2 = points2.mean(axis=0, keepdims=True)
 
     model_index = 1
     iterations = 500
-    max_epsilon = 200
+    max_epsilon = 100
     min_inlier_ratio = 0
     min_num_inlier = 7
     max_trust = 3
-    pointmatchesr = np.array([point1r, point2r])
+    pointmatchesr = (points1, points2)
     model, filtered_matches = ransac.filter_matches(pointmatchesr, model_index, iterations, max_epsilon, min_inlier_ratio, min_num_inlier, max_trust)
     R = model.get_matrix()[0:2,0:2]
     '''
@@ -52,17 +46,15 @@ def main():
     print R
     '''
 
-    plt.figure(1)
-    linelens = []
-    for i in range(0,len(pointmatches)):
-        point1, point2 = pointmatches[i]
-        # point1 = np.matrix(point1 - centroid1).dot(R.T).tolist()[0]
-        point1 = point1 - centroid1
-        point2 = point2 - centroid2
-        plt.plot([point1[0], point2[0]], [point1[1], point2[1]])
-        linelen = ((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2) ** 0.5
-        linelens.append(linelen)
-        axis('equal')
+    new_points1 = np.dot(R, (points1 - centroid1).T).T
+    new_points2 = points2 - centroid2
+    lines = [[p1, p2] for p1, p2 in zip(new_points1, new_points2)]
+    lc = mc.LineCollection(lines)
+    plt.figure()
+    plt.gca().add_collection(lc)
+    plt.scatter(new_points1[:, 0], new_points1[:, 1])
+    plt.gca().autoscale()
+    plt.axis('equal')
     plt.show()
     # plt.hist(linelens)
     # plt.show()
