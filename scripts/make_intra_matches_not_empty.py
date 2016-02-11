108b46183cf549f05c7edee52e6f5b053863214a
Raahil Sha <raahilsha@college.harvard.edu>
2016-02-11 14:13:22 -0500

    Added comments

--- scripts/make_intra_matches_not_empty.py
+++ scripts/make_intra_matches_not_empty.py
@@ -23,12 +23,13 @@ for f in allfiles:
 tilespec = json.loads(open('W04_Sec027.json', 'r').read())
 
 for f in allfiles:
+    # If it isn't empty, just copy-paste it
     if not(f in emptyfiles):
-        #newfilestr = 'intra_25_notempty\\' + f[9:]
-        #json.dump(json.load(open(f, 'r')), open(newfilestr, 'w'), sort_keys=True, indent=4)
-        #del newfilestr
+        newfilestr = 'intra_25_notempty\\' + f[9:]
+        json.dump(json.load(open(f, 'r')), open(newfilestr, 'w'), sort_keys=True, indent=4)
         cooldude = json.load(open(f, 'r'))
     else:
+        # Open files and get the two tilespecs the images come from
         matches = json.loads(open(f, 'r').read())
         img1, img2 = matches[0]['url1'], matches[0]['url2']
         tile1, tile2 = 0, 0
@@ -38,6 +39,7 @@ for f in allfiles:
             if t['mipmapLevels']['0']['imageUrl'] == img2:
                 tile2 = t
         
+        # Determine the region of overlap between the two images
         overlapx_min = max(tile1['bbox'][0], tile2['bbox'][0])
         overlapx_max = max(tile1['bbox'][1], tile2['bbox'][1])
         overlapy_min = max(tile1['bbox'][2], tile2['bbox'][2])
@@ -45,13 +47,15 @@ for f in allfiles:
         obbox = [overlapx_min, overlapx_max, overlapy_min, overlapy_max]
         xrang, yrang = obbox[1] - obbox[0], obbox[3] - obbox[2]
         if xrang < 0 or yrang < 0:
-            print 'They don\'t overlap'
-            
+            print 'They don\'t overlap. Rest of code probably won\'t work'
+        
+        # Choose two random points in the overlap region - one from left and one from right
         xval1 = random.random() * xrang / 2 + obbox[0]
         yval1 = random.random() * yrang + obbox[2]
         xval2 = random.random() * xrang / 2 + obbox[0] + xrang / 2
         yval2 = random.random() * yrang + obbox[2]
         
+        # Add the first point to the matches dict
         newpair = {}
         newpair['dist_after_ransac'] = 1.0
         newp1 = {'l': [xval1 - tile1['bbox'][0],yval1 - tile1['bbox'][2]], 'w': [xval1,yval1]}
@@ -60,6 +64,7 @@ for f in allfiles:
         newpair['p2'] = newp2
         matches[0]["correspondencePointPairs"].append(newpair)
         
+        # Add the second point to the matches dict
         newpair = {}
         newpair['dist_after_ransac'] = 0.0
         newp1 = {'l': [xval2 - tile1['bbox'][0],yval2 - tile1['bbox'][2]], 'w': [xval2,yval2]}
@@ -68,5 +73,6 @@ for f in allfiles:
         newpair['p2'] = newp2
         matches[0]["correspondencePointPairs"].append(newpair)
         
+        # Print out the new matches file
         newfilestr = 'intra_25_notempty\\' + f[9:]
         json.dump(matches, open(newfilestr, 'w'), sort_keys=True, indent=4)
